2cad3f5ee4887796f44304066b53a88a17332759
adriangarciao <garcia.adriann08@gmail.com>
2025-12-02 16:19:34 -0600

    Initial commit

---
+++ src/main/java/com/adriangarciao/traveloptimizer/TraveloptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.adriangarciao.traveloptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveloptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveloptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adriangarciao/traveloptimizer/TraveloptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriangarciao.traveloptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveloptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
